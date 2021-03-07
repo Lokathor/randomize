3c5f1542dbf2a89505ef5a416678ec9897c2e03a
Lokathor <zefria@gmail.com>
2021-03-07 03:44:54 +0000

    Deploying to gh-pages from  @ 896a71d88086f7e05d2fe0c543561d6be8a71c38 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["randomize"] = {"name":"","files":["bounded_rand.rs","dice.rs","free_utils.rs","gen32.rs","lib.rs","os_random.rs","pcg32.rs"]};
+sourcesIndex["randomize"] = {"name":"","dirs":[{"name":"games","files":["exploding_die.rs","standard_die.rs"]}],"files":["bounded_rand.rs","free_utils.rs","games.rs","gen32.rs","lib.rs","os_random.rs","pcg32.rs"]};
 createSourceSidebar();
