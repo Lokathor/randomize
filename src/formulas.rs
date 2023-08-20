76f1a1f673886d0d15fb978cffcbf4a715941769
Lokathor <zefria@gmail.com>
2023-08-20 14:04:59 -0600

    add support for generating unit range floating point values

--- src/formulas.rs
+++ src/formulas.rs
@@ -90,3 +90,120 @@ pub const fn xsl_rr_u128_to_u64(state: u128) -> u64 {
   let folded_bits: u64 = (state ^ (state >> 64)) as u64;
   folded_bits.rotate_right(rot_amount)
 }
+
+/// Returns `k` with probability `2^(-k-1)`, a "binary exponential
+/// distribution".
+pub fn next_binary_exp_distr32<F: FnMut() -> u32>(mut f: F) -> u32 {
+  // Based on a function provided by <https://github.com/orlp>
+
+  // Note(Lokathor): We want to calculate the number of trailing zeroes on a
+  // result `r` from the generator. However, as long as `r` is 0 we act like
+  // it's just an "extra" 32 trailing zeros and then generate again.
+  let mut extra = 0;
+  let mut r: u32 = f();
+  while r == 0 {
+    extra += 1;
+    r = f();
+  }
+  extra * 32 + r.trailing_zeros()
+}
+
+/// Generates an `f32` in the signed or unsigned unit range.
+///
+/// * signed: `[-1.0, 1.0]`
+/// * unsigned: `[0.0, 1.0]`
+pub fn ieee754_random_f32<F: FnMut() -> u32>(mut f: F, signed: bool) -> f32 {
+  // This function provided by <https://github.com/orlp>
+
+  // Returns random number in [0, 1] or [-1, 1] depending on signed.
+  let bit_width = 32;
+  let exponent_bias = 127;
+  let num_mantissa_bits = 23;
+  let num_rest_bits = bit_width - num_mantissa_bits - 1 - signed as i32;
+  let r: u32 = f();
+
+  debug_assert!(num_rest_bits >= 0);
+  debug_assert!(core::mem::size_of::<u32>() * 8 == bit_width as _);
+
+  let mantissa = r >> (bit_width - num_mantissa_bits);
+  let (sign_mask, rand_bit, rest_bits);
+  if signed {
+    sign_mask = r << (bit_width - 1);
+    rand_bit = (r & 2) != 0;
+    rest_bits = (r >> 2) & ((1 << num_rest_bits) - 1);
+  } else {
+    sign_mask = 0;
+    rand_bit = (r & 1) != 0;
+    rest_bits = (r >> 1) & ((1 << num_rest_bits) - 1);
+  }
+
+  // If our mantissa is zero, half of the time we must increase our exponent.
+  let increment_exponent = (mantissa == 0 && rand_bit) as i32;
+
+  // We can usually reuse `rest_bits` to save more calls to the rng.
+  let computed_rest_bits: i32 = if rest_bits > 0 {
+    rest_bits.trailing_zeros() as i32
+  } else {
+    num_rest_bits + next_binary_exp_distr32(&mut f) as i32
+  };
+  let mut exponent: i32 = -1 + increment_exponent - computed_rest_bits;
+
+  // It is very unlikely our exponent is invalid at this point, but keep
+  // regenerating it until it is valid.
+  while exponent < -exponent_bias || exponent > 0 {
+    exponent = -1 + increment_exponent - next_binary_exp_distr32(&mut f) as i32;
+  }
+
+  let final_exponent = ((exponent + exponent_bias) as u32) << num_mantissa_bits;
+  f32::from_bits(sign_mask | final_exponent | mantissa)
+}
+
+/// Generates an `f32` in the signed or unsigned unit range.
+///
+/// * signed: `[-1.0, 1.0]`
+/// * unsigned: `[0.0, 1.0]`
+pub fn ieee754_random_f64<F: FnMut() -> u32>(mut f: F, signed: bool) -> f64 {
+  // This function provided by <https://github.com/orlp>
+
+  // Returns random number in [0, 1] or [-1, 1] depending on signed.
+  let bit_width = 64;
+  let exponent_bias = 1023;
+  let num_mantissa_bits = 52;
+  let num_rest_bits = bit_width - num_mantissa_bits - 1 - signed as i32;
+  let r: u64 = ((f() as u64) << 32) | (f() as u64);
+
+  debug_assert!(num_rest_bits >= 0);
+  debug_assert!(core::mem::size_of::<u32>() * 8 == bit_width as _);
+
+  let mantissa = r >> (bit_width - num_mantissa_bits);
+  let (sign_mask, rand_bit, rest_bits);
+  if signed {
+    sign_mask = r << (bit_width - 1);
+    rand_bit = (r & 2) != 0;
+    rest_bits = (r >> 2) & ((1 << num_rest_bits) - 1);
+  } else {
+    sign_mask = 0;
+    rand_bit = (r & 1) != 0;
+    rest_bits = (r >> 1) & ((1 << num_rest_bits) - 1);
+  }
+
+  // If our mantissa is zero, half of the time we must increase our exponent.
+  let increment_exponent = (mantissa == 0 && rand_bit) as i32;
+
+  // We can usually reuse `rest_bits` to save more calls to the rng.
+  let computed_rest_bits: i32 = if rest_bits > 0 {
+    rest_bits.trailing_zeros() as i32
+  } else {
+    num_rest_bits + next_binary_exp_distr32(&mut f) as i32
+  };
+  let mut exponent: i32 = -1 + increment_exponent - computed_rest_bits;
+
+  // It is very unlikely our exponent is invalid at this point, but keep
+  // regenerating it until it is valid.
+  while exponent < -exponent_bias || exponent > 0 {
+    exponent = -1 + increment_exponent - next_binary_exp_distr32(&mut f) as i32;
+  }
+
+  let final_exponent = ((exponent + exponent_bias) as u64) << num_mantissa_bits;
+  f64::from_bits(sign_mask | final_exponent | mantissa)
+}
