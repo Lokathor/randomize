e07a5268a539c0d6a4a250fc8bc3062657d30de8
Lokathor <zefria@gmail.com>
2023-08-19 21:13:40 -0600

    128-bit stuff

--- src/formulas.rs
+++ src/formulas.rs
@@ -67,14 +67,26 @@ pub const fn lcg128_jump(mul: u128, add: u128, state: u128, mut delta: u128) -> 
   state.wrapping_mul(acc_mult).wrapping_add(acc_plus)
 }
 
-/// Xor-shift high bits then Randomized Rotate, `u64` down to `u32`.
+/// "Xor-shift high bits" then "randomized rotate", `u64` down to `u32`.
 pub const fn xsh_rr_u64_to_u32(state: u64) -> u32 {
   // Note(Lokathor): Bit randomness quality is better in the higher bits. The
   // top 5 bits specify the random rotation, while the next 32 bits are the
   // "source" of the final value. The xor-shift amount is half the total bits in
-  // play, so it's (32+5)/2==18
+  // play, so it's (32+5)/2==18.
   let rot_amount: u32 = (state >> (64 - 5)) as u32;
   let xor_shifted: u64 = state ^ (state >> ((32 + 5) / 2));
   let kept_bits: u32 = (xor_shifted >> (32 - 5)) as u32;
   kept_bits.rotate_right(rot_amount)
+}
+
+/// "Xor-shift low bits" then "randomized rotate", `u128` to `u64`.
+pub const fn xsl_rr_u128_to_u64(state: u128) -> u64 {
+  // Note(Lokathor): Similar ideas to how `xsh_rr_u64_to_u32` works. At 128-bit
+  // size we now use 6 bits to select the random rotation. The xor-shift is by
+  // exactly 64 bits because on modern computers a `u128` will actually be two
+  // 64-bit registers. Having the xor-shift be by exactly 64 bits makes the
+  // operation is a simple `reg1 ^ reg2`.
+  let rot_amount: u32 = (state >> (128 - 6)) as u32;
+  let folded_bits: u64 = (state ^ (state >> 64)) as u64;
+  folded_bits.rotate_right(rot_amount)
 }
