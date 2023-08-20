cb2909de36432f75984ee757ad84e2a4ea2b0481
lokathor <zefria@gmail.com>
2023-08-20 00:43:58 -0600

    remove getrandom for now.

--- src/lib.rs
+++ src/lib.rs
@@ -8,26 +8,20 @@ use formulas::{lcg64_step, xsh_rr_u64_to_u32};
 
 pub mod formulas;
 
+const PCG32_MUL: u64 = 6364136223846793005;
+
 #[derive(Debug, Clone)]
 pub struct PCG32 {
   pub state: u64,
   pub inc: u64,
 }
 impl PCG32 {
-  const MUL: u64 = 6364136223846793005;
-
   pub const fn new(state: u64, inc: u64) -> Self {
     Self { state, inc }
   }
 
-  #[cfg(feature = "getrandom")]
-  pub fn from_os() -> Self {
-    let bytes = [0_u8; size_of::<Self>()];
-    todo!()
-  }
-
   pub fn next_u32(&mut self) -> u32 {
-    let new_state = lcg64_step(Self::MUL, self.inc, self.state);
+    let new_state = lcg64_step(PCG32_MUL, self.inc, self.state);
     let out = xsh_rr_u64_to_u32(self.state);
     self.state = new_state;
     out
@@ -41,20 +35,12 @@ pub struct PCG32X<const K: usize> {
   pub ext: [u32; K],
 }
 impl<const K: usize> PCG32X<K> {
-  const MUL: u64 = 6364136223846793005;
-
   pub const fn new(state: u64, inc: u64, ext: [u32; K]) -> Self {
     Self { state, inc, ext }
   }
 
-  #[cfg(feature = "getrandom")]
-  pub fn from_os() -> Self {
-    let bytes = [0_u8; size_of::<Self>()];
-    todo!()
-  }
-
   pub fn next_u32(&mut self) -> u32 {
-    let new_state = lcg64_step(Self::MUL, self.inc, self.state);
+    let new_state = lcg64_step(PCG32_MUL, self.inc, self.state);
     let ext_index: usize = self.state as usize % K;
     let out = xsh_rr_u64_to_u32(self.state) ^ self.ext[ext_index];
     let mut carry = new_state == 0;
